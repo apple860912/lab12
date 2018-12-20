d34754e45a6685b933db19e85eee59e11ce9c58d
apple860912 <love442727@gmail.com>
2018-12-20 14:44:33 +0800

    first commit

---
+++ app/src/main/java/com/example/user/myapplication/Data.java
@@ -0,0 +1,18 @@
+package com.example.user.myapplication;
+
+import javax.xml.transform.Result;
+
+public class Data {
+    Result result;
+
+    class Result{
+        Result[] results;
+        public String Station;
+        public String Destination;
+
+        class Results{
+            String Station;
+            String Destination;
+        }
+    }
+}
